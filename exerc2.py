2ea32a059ba8d1cce7b43eb1ad1e9667be488f9b
Vitor Bortoloto <70710855+VitorBortoloto@users.noreply.github.com>
2021-09-01 16:01:55 -0300

    Delete exerc2.py

--- exerc2.py
+++
@@ -1,7 +0,0 @@
-def somaImposto(taxaImposto, custo):
-
-    valor_imposto = custo + (custo * taxaImposto / 100)
-
-    return valor_imposto
-
-print ('O valor do produto com imposto é: R$', somaImposto(20, 100))
