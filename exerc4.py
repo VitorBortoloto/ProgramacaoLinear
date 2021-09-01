266940a308bf2eb2a8ca8a697c54712dd2e48993
Vitor Bortoloto <70710855+VitorBortoloto@users.noreply.github.com>
2021-09-01 16:02:06 -0300

    Delete exerc4.py

--- exerc4.py
+++
@@ -1,6 +0,0 @@
-cpf = input("Digite o seu CPF no formato xxx.xxx.xxx-xx: ")
-
-while(cpf[3] !=".") or (cpf[7] !=".") or (cpf[11] !="-"):
-    cpf = input("O 'CPF' pricisa estar no formato (xxx.xxx.xxx-xx) :")
-else:
-    print("O 'CPF' está no formato correto")
